4cc13f9040bb43df9d96f840892158ebd3d061fc
z.esmaeilpour <z.esmaeilpour49@gmail.com>
2025-10-05 17:31:19 +0330

    Update Next.js configuration and dependencieS

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["clasor-content-preview", "yet-another-react-lightbox"],
+  experimental: {
+    esmExternals: 'loose'
+  },
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'clasor-content-preview$': 'clasor-content-preview/dist/clasor-content-preview.esm.js',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
